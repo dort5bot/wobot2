56251f20fd8dc6dfa11467bcda16ad61feb7c2ee
dort5bot <dersdep@gmail.com>
2025-08-28 12:41:57 +0300

    Update worker_a.py

--- jobs/worker_a.py
+++ jobs/worker_a.py
@@ -2,8 +2,6 @@
 # veri toplayıcı Queue
 # Worker A: ticker ve funding verilerini cache’e koyar (async)
 # Config üzerinden interval ve sembol listesi kontrolü
-
-# jobs/worker_a.py
 import asyncio
 import logging
 from utils.config import CONFIG
@@ -19,24 +17,52 @@ class WorkerA:
         self.client = BinanceClient()
         self.stream_mgr = StreamManager(self.client, loop=self.loop)
         self._running = False
+        self._tasks: list[asyncio.Task] = []
 
-    def start(self):
+    async def start_async(self):
+        if self._running:
+            return
         self._running = True
-        streams = [f"{s.lower()}@kline_{CONFIG.BINANCE.STREAM_INTERVAL}" for s in CONFIG.BINANCE.TOP_SYMBOLS_FOR_IO]
+
+        streams = [f"{s.lower()}@kline_{CONFIG.BINANCE.STREAM_INTERVAL}" 
+                   for s in CONFIG.BINANCE.TOP_SYMBOLS_FOR_IO]
+
+        # Stream başlat
         self.stream_mgr.start_combined_groups(streams, self.bridge)
         LOG.info("WorkerA started: %s", streams)
 
-        self.stream_mgr.start_periodic_funding_poll(
-            CONFIG.BINANCE.TOP_SYMBOLS_FOR_IO,
-            interval_sec=60,
-            callback=lambda data: asyncio.create_task(self.queue.put({"funding": data}))
-        )
+        # Funding poller
+        async def funding_loop():
+            while self._running:
+                try:
+                    data = await self.client.get_funding_rates(CONFIG.BINANCE.TOP_SYMBOLS_FOR_IO)
+                    await self.queue.put({"funding": data})
+                except asyncio.CancelledError:
+                    raise
+                except Exception:
+                    LOG.exception("WorkerA funding poll error")
+                await asyncio.sleep(60)
+
+        task = asyncio.create_task(funding_loop(), name="worker_a_funding")
+        self._tasks.append(task)
 
     async def bridge(self, msg):
         """Stream mesajlarını queue'ya aktarır"""
-        await self.queue.put(msg)
+        try:
+            await self.queue.put(msg)
+        except Exception:
+            LOG.exception("WorkerA bridge error")
 
-    def stop(self):
+    async def stop_async(self):
+        if not self._running:
+            return
         self._running = False
         self.stream_mgr.cancel_all()
+        for t in self._tasks:
+            t.cancel()
+            try:
+                await t
+            except asyncio.CancelledError:
+                pass
+        self._tasks.clear()
         LOG.info("WorkerA stopped")
