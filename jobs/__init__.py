e0bd82ab3e8516be3b94ec829af0b67da8d37d1f
dort5bot <dersdep@gmail.com>
2025-08-28 12:43:54 +0300

    Create __init__.py

---
+++ jobs/__init__.py
@@ -0,0 +1,12 @@
+"""
+jobs package
+------------
+Bu sayede from jobs import WorkerA, WorkerB, WorkerC şeklinde merkezi import yapılabilir
+Worker sınıflarını merkezi import ile kolay erişim sağlar.
+"""
+
+from .worker_a import WorkerA
+from .worker_b import WorkerB
+from .worker_c import WorkerC
+
+__all__ = ["WorkerA", "WorkerB", "WorkerC"]
